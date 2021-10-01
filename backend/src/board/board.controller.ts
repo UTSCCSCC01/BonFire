fde0f94c19c032d07eb0a26a18ea1cc93359a64f
davepetrov <davidpetrovx@gmail.com>
2021-09-30 22:15:09 -0400

    Added Board controller

---
+++ backend/src/board/board.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, HttpException, HttpStatus, Param, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiOkResponse, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import { Board, User } from '@prisma/client';
+import { RequestUser } from 'src/auth/auth.service';
+import { BoardService } from './board.service';
+
+@Controller('board')
+@ApiTags('board')
+@UseGuards(AuthGuard())
+@ApiBearerAuth() 
+export class BoardController {
+    constructor(private readonly boardService: BoardService) {}
+
+    @Post()
+    @ApiOperation({ summary: 'Creates and returns a new board' })
+    @ApiParam({ name: 'title' })
+    public async createBoard(@RequestUser() user: User, @Body() board: Board): Promise<Board>{
+        return this.boardService.create(user, board);
+    }
+
+}
