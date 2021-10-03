fecc0ee6459cfea42f83b6befa0638fe04a14bc9
davepetrov <davidpetrovx@gmail.com>
2021-10-03 16:39:14 -0400

    Added AuthModule as import to board, state, card

--- backend/src/state/state.module.ts
+++ backend/src/state/state.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
 import { PrismaService } from 'src/prisma.service';
 import { StateService } from 'src/state/state.service';
 import { StateController } from './state.controller';
@@ -6,5 +7,6 @@ import { StateController } from './state.controller';
 @Module({
   controllers: [StateController],
   providers: [StateService, PrismaService],
+  imports: [AuthModule],
 })
 export class StateModule {}
