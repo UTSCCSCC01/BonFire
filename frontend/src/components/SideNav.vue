eeb00316e3769b8055fa343815acfa7825d2021b
ChristianChenL <50886158+ChristianChenL@users.noreply.github.com>
2021-10-05 18:13:23 -0500

    Implemented loop over user boards
    Implemented loop over user boards to display all the user boards in the sidebar

--- frontend/src/components/SideNav.vue
+++ frontend/src/components/SideNav.vue
@@ -44,11 +44,11 @@
         nav
         dense
       >
-        <v-list-item to="/board/1">
+        <v-list-item v-for="board in boards" :key="board.id" :to="`/board/${board.id}`"> 
           <v-list-item-icon>
             <v-icon>fas fa-fire</v-icon>
           </v-list-item-icon>
-          <v-list-item-title>Personal Board 1</v-list-item-title>
+          <v-list-item-title>{{board.title}}</v-list-item-title>
         </v-list-item>
       </v-list>
 
@@ -66,15 +66,18 @@
 
 <script>
 import { logout } from '@/plugins/axios';
+//import axios from '@/plugins/axios'
 
 export default {
 	data() {
 		return {
-			drawer: []
+			drawer: [],
+      boards: [] 
 		}
 	},
 	mounted() {
-		console.log(this.currentUser, this.isAuthenticated)
+		console.log(this.currentUser, this.isAuthenticated);
+    this.getUserBoards();
 	},
 	methods: {
     signOut() {
