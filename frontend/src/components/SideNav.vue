cb57589a54bb4e8729e4b698796dcef73b3efb75
Christian Chen Liu <78563747+ChristianChenLiu@users.noreply.github.com>
2021-10-07 19:02:31 -0400

    feat(frontend): [WRLD-157] Display user boards (#26)
    * Created getUserBoards function
    
    Created getUserBoards function in the vue file in order to get the user boards
    
    * Implemented loop over user boards
    
    Implemented loop over user boards to display all the user boards in the sidebar
    
    * Display user board list
    
    Display user board list
    
    Co-authored-by: ChristianChenL <50886158+ChristianChenL@users.noreply.github.com>

--- frontend/src/components/SideNav.vue
+++ frontend/src/components/SideNav.vue
@@ -44,11 +44,15 @@
         nav
         dense
       >
-        <v-list-item to="/board/1">
+        <v-list-item
+          v-for="board in boards"
+          :key="board.id"
+          :to="`/board/${board.id}`"
+        > 
           <v-list-item-icon>
             <v-icon>fas fa-fire</v-icon>
           </v-list-item-icon>
-          <v-list-item-title>Personal Board 1</v-list-item-title>
+          <v-list-item-title>{{ board.title }}</v-list-item-title>
         </v-list-item>
       </v-list>
 
@@ -69,18 +73,31 @@ export default {
   data() {
     return {
       drawer: [],
+      boards: [],
     };
   },
   mounted() {
-    this.$http.get('boards');
+    this.getUserBoards();
   },
   methods: {
     signOut() {
       localStorage.removeItem('token');
       this.$router.push('/');
     },
+
+    getUserBoards() {
+      this.$http.get('boards')
+      .then(res => {
+        this.boards = res.data;
+      })
+      .catch(err => {
+        console.error(err);
+      })
+    },
+
   },
 };
+
 </script>
 
 <style>
