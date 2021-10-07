9ba7c115b60f6677da8486aef8ba142d9a5130a6
Armand Sarkezians <50157315+ArmandSarkezians@users.noreply.github.com>
2021-10-07 19:14:22 -0400

    feat(frontend):[WRLD-137] Created Landing Page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,81 @@
 <template>
   <div>
     <v-app id="inspire">
-      <!-- Navbar -->
-      <div class="nav">
-        <v-app-bar
-          v-if="$route.meta.noAuthRequired"
-          app
+      <v-content class="white">
+        <!-- Navbar -->
+        <div class="nav">
+          <v-app-bar
+            app
+            color="orange darken-3"
+            height="90"
+            flat
+            v-if="$route.meta.noAuthRequired"
+          >
+            <v-container class="py-0 fill-height">
+              <v-col cols="1">
+                <v-img
+                  src="../src/assets/img/logo.png"
+                  contain="true"
+                  height="65"
+                  width="65"
+                >
+                </v-img>
+              </v-col>
+              <v-card-title class="py-0"> BonFire </v-card-title>
+              <v-btn v-for="link in links" :key="link" :to="link.to" text>
+                {{ link.name }}
+              </v-btn>
+
+              <v-spacer />
+            </v-container>
+          </v-app-bar>
+          <v-sidenav v-else> </v-sidenav>
+        </div>
+
+        <!-- Main -->
+        <div class="container">
+          <router-view :currentUser="currentUser" class="router-view" />
+        </div>
+
+        <!-- Footer -->
+        <v-footer
           color="white"
-          flat
+          v-if="$route.meta.noAuthRequired"
+          absolute="true"
         >
-          <v-container class="py-0 fill-height">
-            <v-avatar
-              class="mr-10"
-              color="grey darken-1"
-              size="32"
-            />
-            <v-app-bar-title class="py-0">
-              BonFire
-            </v-app-bar-title>
-
-            <v-btn
-              v-for="link in links"
-              :key="link.name"
-              :to="link.to"
-              text
-            >
-              {{ link.name }}
-            </v-btn>
-
-            <v-spacer />
-          </v-container>
-        </v-app-bar>
-        <v-sidenav v-else />
-      </div>
-
-      <!-- Main -->
-      <div class="container">
-        <router-view class="router-view" />
-        <notifications position="bottom" />
-      </div>
-
-      <!-- Footer -->
-      <v-footer
-        v-if="$route.meta.noAuthRequired"
-        color="primary lighten-1"
-      >
-        <v-row justify="center">
-          <v-col
-            class="primary lighten-1 text-center"
-            cols="12"
-          >
-            About Something
-          </v-col>
-          <v-col
-            class="primary lighten-2 py-4 text-center white--text"
-            cols="12"
-          >
-            {{ new Date().getFullYear() }} — <strong>Hello World - 418</strong>
-          </v-col>
-        </v-row>
-      </v-footer>
+          <v-row justify="center">
+            <v-col class="orange darken-3 text-center white--text" cols="12">
+              About Us
+            </v-col>
+            <v-col class="orange darken-4 text-center white--text" cols="12">
+              {{ new Date().getFullYear() }} <strong>Hello World - 418</strong>
+            </v-col>
+          </v-row>
+        </v-footer>
+      </v-content>
     </v-app>
   </div>
 </template>
 <script>
-  import SideNav from './components/SideNav';
+import SideNav from "./components/SideNav";
 
-  export default {
-    components: {
-      'v-sidenav': SideNav
-    },
-    data() {
-      return {
-        links: [
-          { name: 'Welcome', to: '/'},
-          { name: 'Register', to: '/register'},
-          { name: 'Signin', to: '/signin'},
-        ],
-      }
-    },
-  }
+export default {
+  components: {
+    "v-sidenav": SideNav,
+  },
+  data() {
+    return {
+      links: [
+        { name: "Welcome", to: "/" },
+        { name: "Register", to: "/register" },
+        { name: "Sign In", to: "/signin" },
+      ],
+    };
+  },
+  mounted() {
+    console.log(this.links);
+  },
+};
 </script>
 <style>
 #app {
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,29 +1,50 @@
 <template>
-  <div class="home">
-    <v-main class="grey lighten-3">
+    <div class="home">
       <v-container>
-        <v-row>
-          <v-col>
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-            >
-              <img
-                alt="Vue logo"
-                src="../assets/logo.png"
-              >
-              <!--  -->
-            </v-sheet>
-          </v-col>
-        </v-row>
+        <br><br>
+        
+        <center>
+          <h1 class="display-3 font-weight-black">Welcome to Bonfire, </h1>
+          <h1 class="display-1 font-weight-black"> The Student Designed Organization Board </h1>
+          <br>
+          <h1 class="subtitle-2 font-weight-black"> Create, customize, and organize your schedule with Bonfire Cards. Take your education into your own hands by registering today.</h1>
+          <br><br><br><br><br>
+  
+          <v-btn v-for="link in links" :key="link" :to="link.to" text>
+            {{ link.name }}
+          </v-btn>
+        </center>
+        <br><br><br><br><br><br>
       </v-container>
-    </v-main>
-  </div>
-</template>
-
-<script>
-
-export default {
-  components: { },
-};
-</script>
+  
+      <v-layout align-center justify-center>
+        <v-img src="../assets/img/create.png" contain="true" height="150" width="150"> </v-img>
+        <v-img src="../assets/img/customize.png" contain="true" height="150" width="150"> </v-img>
+        <v-img src="../assets/img/organize.png" contain="true" height="150" width="150"> </v-img>
+      </v-layout>
+  
+      <br><br>
+      <v-layout align-center justify-center>
+        Create your own classes, boards, and more &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+        Customize your work, spaces, and more &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        Organize your school or work life &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      </v-layout>
+  
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        links: [
+          { name: "Register", to: "/register" },
+          { name: "Sign In", to: "/signin" },
+        ],
+      };
+    },
+    mounted() {
+      console.log(this.links);
+    },
+  };
+  </script>
