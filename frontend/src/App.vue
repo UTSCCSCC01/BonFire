4934504bf1946f2001fa1750aad2d33ced0dad72
Armand Sarkezians <50157315+ArmandSarkezians@users.noreply.github.com>
2021-10-07 20:59:06 -0400

    Use template for Landing Page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,11 +5,11 @@
         <!-- Navbar -->
         <div class="nav">
           <v-app-bar
+            v-if="$route.meta.noAuthRequired"
             app
-            color="orange darken-3"
+            color="#FBE7D3"
             height="90"
             flat
-            v-if="$route.meta.noAuthRequired"
           >
             <v-container class="py-0 fill-height">
               <v-col cols="1">
@@ -18,40 +18,55 @@
                   contain="true"
                   height="65"
                   width="65"
-                >
-                </v-img>
+                />
               </v-col>
-              <v-card-title class="py-0"> BonFire </v-card-title>
-              <v-btn v-for="link in links" :key="link" :to="link.to" text>
+              <v-card-title class="py-0">
+                BonFire
+              </v-card-title>
+              <v-btn
+                v-for="link in links"
+                :key="link"
+                :to="link.to"
+                text
+              >
                 {{ link.name }}
               </v-btn>
 
               <v-spacer />
             </v-container>
           </v-app-bar>
-          <v-sidenav v-else> </v-sidenav>
+          <v-sidenav v-else />
         </div>
 
         <!-- Main -->
-        <div class="container">
-          <router-view :currentUser="currentUser" class="router-view" />
+        <div>
+          <router-view
+            :current-user="currentUser"
+            class="router-view"
+          />
         </div>
 
-        <!-- Footer -->
+        <!-- Footer
         <v-footer
-          color="white"
           v-if="$route.meta.noAuthRequired"
+          color="white"
           absolute="true"
         >
           <v-row justify="center">
-            <v-col class="orange darken-3 text-center white--text" cols="12">
+            <v-col
+              class="orange darken-3 text-center white--text"
+              cols="12"
+            >
               About Us
             </v-col>
-            <v-col class="orange darken-4 text-center white--text" cols="12">
+            <v-col
+              class="orange darken-4 text-center white--text"
+              cols="12"
+            >
               {{ new Date().getFullYear() }} <strong>Hello World - 418</strong>
             </v-col>
           </v-row>
-        </v-footer>
+        </v-footer> -->
       </v-content>
     </v-app>
   </div>
