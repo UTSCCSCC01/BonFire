c078642f2384b25fc52aa39482af27322dd84a28
AhmedHalat <ahmedhalat@gmail.com>
2021-10-20 20:05:52 -0400

    feat: creating state reorder endpoint

--- backend/src/board/board.module.ts
+++ backend/src/board/board.module.ts
@@ -3,10 +3,11 @@ import { BoardService } from './board.service';
 import { BoardController } from './board.controller';
 import { PrismaService } from 'src/prisma.service';
 import { AuthModule } from 'src/auth/auth.module';
+import { StateService } from 'src/state/state.service';
 
 @Module({
   controllers: [BoardController],
-  providers: [BoardService, PrismaService],
+  providers: [BoardService, PrismaService, StateService],
   imports: [AuthModule],
 })
 export class BoardModule {}
--- backend/src/board/board.service.ts
+++ backend/src/board/board.service.ts
@@ -1,6 +1,7 @@
 import { Board, Prisma } from '.prisma/client';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
+import e from 'express';
 import { StateDto } from 'src/constants/state';
 import { PrismaService } from 'src/prisma.service';
 import { BoardDto } from '../constants/board';
@@ -95,6 +96,64 @@ export class BoardService {
     };
     return this.prisma.state.findMany({
       where: stateWhereInput,
+      orderBy: [
+        {
+          order: 'asc',
+        },
+      ],
     });
+  }
+
+  /**
+   * Reorganize board states
+   *
+   * @param {number} id
+   * @param {number} oldIndex Previous state index
+   * @param {number} newIndex New state index
+   */
+  async reorderStates(user: User, id: number, oldIndex: any, newIndex: any) {
+    let states = await this.findStates(user, id);
+
+    if (oldIndex == newIndex) {
+      throw new HttpException('The state index did not change', 400);
+    } else if (oldIndex < 0 || newIndex < 0) {
+      throw new HttpException('State index cannot be negative', 400);
+    } else if (oldIndex > states.length || newIndex > states.length) {
+      throw new HttpException('State index cannot be greater than number of states', 400);
+    }
+
+    states = states.sort((a, b) => {
+      if (a.type === 'TODO') return -1;
+      if (a.type === 'DONE') return 1;
+
+      if (b.type === 'TODO') return 1;
+      if (b.type === 'DONE') return -1;
+
+      return a.order - b.order;
+    });
+
+    console.log({ before: states })
+
+    let moved = states.splice(newIndex, 1);
+    states.splice(oldIndex, 0, moved[0]);
+
+    console.log({ after: states })
+
+    const promises = [];
+    states.forEach((state, i) => {
+
+      promises.push(this.prisma.state.update({
+        where: {
+            id: state.id,
+        },
+        data: {
+          order: i - 1
+        }
+      }))
+    });
+
+    await Promise.all(promises);
+
+    return;
   }
 }
