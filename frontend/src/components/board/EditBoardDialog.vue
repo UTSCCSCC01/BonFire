41eddd2f03b31487d5e7e1d0203a035e14fa1e03
davepetrov <davidpetrovx@gmail.com>
2021-10-27 10:38:03 -0400

    Revert "Revert "Add ability to delete class as teacher""
    This reverts commit d8444f6aaaffded857b9a5e114d395ed09821fda.

--- frontend/src/components/board/EditBoardDialog.vue
+++ frontend/src/components/board/EditBoardDialog.vue
@@ -123,7 +123,7 @@ export default {
       if (confirmation) {
         this.$http.delete(`boards/${board.id}`)
         .then(() => {
-           if (board.id == this.$route.params.boardId) this.$router.push({ name: 'Dashboard' });
+           this.$router.push({ name: 'Dashboard' });
         })
         .catch(err => {
           console.error(err);
